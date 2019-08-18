f0f3f3820131fd8c6c4bbb2acde7b01888bd2c24
mrdevgo <mrdevgo@gmail.com>
2019-08-18 11:05:56 +0300

    Set из котов. Работа с Set

---
+++ 1.JavaSyntax/src/com/javarush/task/task08/task0819/Solution.java
@@ -0,0 +1,40 @@
+package com.javarush.task.task08.task0819;
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+/* 
+Set из котов
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+        Set<Cat> cats = createCats();
+        for (Cat cat : cats) {
+            cats.remove(cat);
+            break;
+        }
+        printCats(cats);
+    }
+
+    public static Set<Cat> createCats() {
+        Set<Cat> cats = new HashSet<>();
+        cats.add(new Cat());
+        cats.add(new Cat());
+        cats.add(new Cat());
+        return cats;
+    }
+
+    public static void printCats(Set<Cat> cats) {
+        Iterator<Cat> iterator = cats.iterator();
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
+    }
+
+    public static class Cat {
+    }
+}
