34f8f77cb8bc928b25ceb32d2fee950b94d2b49c
mrdevgo <mrdevgo@gmail.com>
2019-08-20 14:51:56 +0300

    Свой FileWriter

--- 2.JavaCore/src/com/javarush/task/task19/task1917/FileConsoleWriter.java
+++ 2.JavaCore/src/com/javarush/task/task19/task1917/FileConsoleWriter.java
@@ -4,9 +4,73 @@ package com.javarush.task.task19.task1917;
 Свой FileWriter
 */
 
+import java.io.File;
+import java.io.FileDescriptor;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class FileConsoleWriter {
-    public static void main(String[] args) {
+
+    private FileWriter fileWriter;
+
+    public FileConsoleWriter(String fileName) throws IOException {
+        fileWriter = new FileWriter(fileName);
+    }
+
+    public FileConsoleWriter(String fileName, boolean append) throws IOException {
+        fileWriter = new FileWriter(fileName, append);
+    }
+
+    public FileConsoleWriter(File file) throws IOException {
+        fileWriter = new FileWriter(file);
+    }
+
+    public FileConsoleWriter(File file, boolean append) throws IOException {
+        fileWriter = new FileWriter(file, append);
+    }
+
+    public FileConsoleWriter(FileDescriptor fd) {
+        fileWriter = new FileWriter(fd);
+    }
+
+
+    public static void main(String[] args) throws IOException {
 
     }
+
+    public void write(char[] cbuf, int off, int len) throws IOException {
+        fileWriter.write(cbuf, off, len);
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < cbuf.length; i++) {
+            stringBuilder.append(cbuf[i]);
+        }
+        String s = stringBuilder.toString();
+        System.out.println(s.substring(off, off+len));
+    }
+
+    public void write(int c) throws IOException {
+        fileWriter.write(c);
+        System.out.println(c);
+    }
+
+    public void write(String str) throws IOException {
+        fileWriter.write(str);
+        System.out.println(str);
+    }
+
+    public void write(String str, int off, int len) throws IOException {
+        fileWriter.write(str, off, len);
+        System.out.println(str.substring(off,off+len));
+    }
+
+    public void write(char[] cbuf) throws IOException {
+        fileWriter.write(cbuf);
+        System.out.println(cbuf);
+    }
+
+    public void close() throws IOException {
+        fileWriter.close();
+    }
+
 
 }
