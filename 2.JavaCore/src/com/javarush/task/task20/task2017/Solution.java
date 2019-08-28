92a37442debcd74d85246a77b2cffa7401781bb1
mrdevgo <mrdevgo@gmail.com>
2019-08-28 09:52:28 +0300

    2020. Serializable. Сериализация человека

---
+++ 2.JavaCore/src/com/javarush/task/task20/task2017/Solution.java
@@ -0,0 +1,32 @@
+package com.javarush.task.task20.task2017;
+
+import java.io.ObjectInputStream;
+import java.io.Serializable;
+
+/* 
+Десериализация
+*/
+public class Solution {
+    public A getOriginalObject(ObjectInputStream objectStream)  {
+        try {
+            A a = (A) objectStream.readObject();
+            return a;
+        } catch (Exception e) {
+            System.out.println(e);
+            return null;
+        }
+    }
+
+    public class A implements Serializable {
+    }
+
+    public class B extends A {
+        public B() {
+            System.out.println("inside B");
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
